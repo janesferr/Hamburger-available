125199a6d393ce09b627dc7248a8b0d9b84b285c
jansferr <sferrazzajane@gmail.com>
2021-02-19 13:34:56 -0500

    good one

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,13 @@
+use rrg6gyh3ka7sv7f8;
+
+CREATE TABLE cats
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	sleepy BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
 INSERT INTO cats (name) VALUES ('Molly');
 INSERT INTO cats (name) VALUES ('Charlie');
 INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
