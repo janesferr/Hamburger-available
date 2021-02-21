1d14180eaccb3f7d42caa7adc38a18c608eb29cd
jansferr <sferrazzajane@gmail.com>
2021-02-21 15:09:30 -0500

    added a pic and color

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if(process.env.JAWSDB_URL){
     user: 'root',
     // NOTE: Be sure to add your MySQL password here!
     password: 'Welcome1',
-    database: 'rrg6gyh3ka7sv7f8',
+    database: 'burgers_db' || 'rrg6gyh3ka7sv7f8',
 
 });
 }
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static('public'));
+//set the path for the image details
+app.use(express.static('views/images'));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +18,9 @@ const exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+ 
+
 
 // Import routes and give the server access to them.
 const routes = require('./controllers/burgersController.js');
